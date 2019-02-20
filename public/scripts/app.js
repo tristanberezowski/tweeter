355ffa0c54f08f23218ce8927b3e023adc6ff5c3
Tristan <tristanberezowski@gmail.com>
2019-02-20 05:47:28 +0000

    working on app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+function createTweetElement(tweet) {
+  const $article = $("<article>").addClass("tweet");
+  const $header = $("<header>");
+  const $img = $("<img>").attr('src', tweet.user.avatars.small);
+  const $h5 = $("<h5>");
+  const $handle = $("<div>");
+  const $footer = $("<footer>");
+  const $datepassed = $("<div>");
+}
