2500b3be31aae89af22677de8186f80a1c2c516f
Tristan <tristanberezowski@gmail.com>
2019-02-20 02:11:37 +0000

    implemented new tweet functionality and making display tweet css and html template

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $('#new-tweet-textarea').on('input', function() {
+  let newVal = 140 - $(this).val().length;
+  $(this).siblings('.counter').text(newVal);
+
+  if(Number($(this).siblings('.counter').text()) < 0) {
+    $(this).siblings('.counter').css('color', 'red');
+  }
+  else {
+    $(this).siblings('.counter').css('color', '');
+  }
+  });
+
+  $('#new-tweet-form').on('submit', function(event) {
+    event.preventDefault();
+    $('#new-tweet-textarea').val('');
+    $(this).children('.counter').text('140');
+  });
+
+
+})
